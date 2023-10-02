de26b33a197d127a7ea560baadfaf55f51ff6613
Tufiq <tufivic@gmail.com>
2023-10-02 02:27:46 -0300

    se cargaron datos y se actualizaron estilos

--- src/component/views/Inicio/InfoRestaurant.jsx
+++ src/component/views/Inicio/InfoRestaurant.jsx
@@ -12,19 +12,17 @@ export default function InfoRestaurant() {
         <aside className="info">
           <h1>La campiña</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-            velit cupiditate sunt debitis aut nemo a cum, obcaecati voluptas
-            laudantium officia voluptates soluta porro maiores ut perspiciatis
-            aspernatur, enim molestias. Excepturi accusamus quibusdam
-            perferendis praesentium harum architecto omnis laborum iusto magnam,
-            eligendi, natus accusantium vel neque molestias officiis, vitae
-            recusandae perspiciatis voluptatum cumque expedita adipisci?
-            Explicabo deleniti veniam cupiditate esse sequi sint repellat ad,
-            temporibus ipsum, debitis doloremque asperiores molestiae dolore
-            exercitationem. Suscipit molestiae accusantium sed atque aut
-            architecto, corporis saepe, minima eaque temporibus explicabo,
-            obcaecati voluptatum eveniet alias odio vero soluta adipisci sunt
-            culpa labore hic! Eum, nesciunt.
+          ¡Bienvenido a La Campiña!
+
+En nombre de todo nuestro equipo, queremos darte una cálida bienvenida a nuestra página gourmet. Aquí, no solo encontrarás exquisitas delicias culinarias, sino también un rincón donde la pasión por la programación se fusiona con el amor por el buen comer.
+
+Somos un grupo consolidado de apasionados de la programación, pero también somos amantes de la gastronomía. Nuestra misión es combinar estas dos pasiones para crear una experiencia única para ti. Queremos acercarte al mundo de la buena comida de una manera especial, a través de recetas, consejos, y por supuesto, un toque de tecnología.
+
+Nos hemos reunido en este proyecto con el propósito de llevar a cabo una página gourmet que no solo refleje nuestro amor por la programación, sino también nuestro deseo de compartir la alegría de disfrutar de exquisitos platos y experiencias culinarias memorables. Aquí, no solo encontrarás recetas y consejos, sino también historias y pasión por el buen comer.
+
+Así que, querido usuario, prepárate para explorar un mundo de sabores y conocimientos culinarios con nosotros. Juntos, trabajamos cada día para mejorar tu experiencia y acercarte a la gastronomía de una manera única. ¡Bienvenido a La Campiña, donde la programación y el buen comer se unen para ofrecerte lo mejor de ambos mundos!
+
+Si tienes alguna pregunta, sugerencia o simplemente quieres compartir tus pensamientos, no dudes en ponerte en contacto con nosotros. Estamos aquí para ti, ¡disfruta de tu visita!.
           </p>
           <div className="mt-4">
             <button
